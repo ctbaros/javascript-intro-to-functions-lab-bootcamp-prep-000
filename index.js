3f7808024cb5631a63f7b8a8bfa26836d0fb4373
Christian Baros <ctbaros@gmail.com>
2019-04-01 20:13:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHitoGrandma(string) {
-  if (string.toLowerCase() === string)
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!" 
+  }
   
