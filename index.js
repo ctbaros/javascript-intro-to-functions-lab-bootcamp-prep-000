bbd04bcb29a7eca6321a983feb9717bd9b50c26b
Christian Baros <ctbaros@gmail.com>
2019-04-01 20:06:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(hello) {
+  return hello.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHitoGrandma(string) {
+  if (string.toLowercase() === string) {
+    return "I cant hear you!"
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  } 
+  
+  if (string === "I love you, Grandma."()) {
+    return "I love you, too" 
+    }
