49d894ad190e102d1a8968a200a82d048bf12bfb
Christian Baros <ctbaros@gmail.com>
2019-04-01 20:08:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,12 +18,3 @@ function sayHitoGrandma(string) {
   if (string.toLowercase() === string) {
     return "I cant hear you!"
 }
-
-function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you!"
-  } 
-  
-  if (string === "I love you, Grandma."()) {
-    return "I love you, too" 
-    }
